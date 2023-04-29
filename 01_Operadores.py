7a2f76fa1cae57a4924ffda318c0754ccd9cb9b8
IvanPC39 <ip110697@gmail.com>
2023-04-29 16:16:01 -0600

    Operadores aritmeticos

---
+++ 01_Operadores.py
@@ -0,0 +1,27 @@
+#Comentaré más seguido para acostumbrarme a hacerlo
+# Operadores aritmeticos
+
+print(3 + 9)
+print(3 - 9)
+print(3 / 9)
+print(3 * 9) 
+print(10 % 3) # Operador de modulo, se usa para saber el resto de una división
+print(10 // 3) # Da como resultado el núnmero entero más cercano al resultado de la división
+print(2 ** 3) # Sirve para exponenciar los numeros (2 elevado a 3)
+
+# Operadores comparativos
+
+print(3 > 9) # mayor que
+print(3 < 9) # menor que
+print(3 >= 9) # mayor o igual que
+print(3 <= 9) # menor o igual que
+print(3 == 9) # igual que
+print(3 != 9) # distinto que
+
+""" Operadores lógicos
+Los operadores lógicos son "and, or, not" 
+y en otros lenguajes se ponen como "&& (and), || (or), != (not)"""
+
+print(3 > 9 and 2 > 1) 
+print(3 < 9 or 4 >= 5)
+print(not (3 != 9)) # not es para negar la condición
