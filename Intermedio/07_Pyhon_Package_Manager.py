0fad60d72bee83de93913766acbe58fab002c6ae
IvanPC39 <ip110697@gmail.com>
2023-05-25 15:15:55 -0600

    Manejo de Paquetes

---
+++ Intermedio/07_Pyhon_Package_Manager.py
@@ -0,0 +1,28 @@
+# Manejo de Paquetes
+
+# PIP  https://pypi.org
+
+# Las instalaciones se pueden hacer desde la consola en terminal
+
+# pip install pip
+
+# pip --version #Para ver que version tienes instalada
+
+# pip install nombre del paquete #Para instalar los paquetes que necesites
+
+import numpy # pip install numpy
+
+print(numpy.version.version)
+
+numpy_array = numpy.array([25, 20, 23, 28, 35])
+print(type(numpy_array))
+
+print(numpy_array * 2)
+
+import pandas # pip install pandas
+
+# pip list # Ves el listado de todos los paquetes instalados
+
+# pip uninstall nombre del paquete # Para desinstalar paquetes
+
+# pip show nombre del paquete # Muestra detalles sobre el paquete
