dbb8ec78f06cec1d773a4f203d4cd77da7c06021
IvanPC39 <ip110697@gmail.com>
2023-05-29 15:58:42 -0600

    6 practicas

--- Practicas/01_Practicas.py
+++ Practicas/01_Practicas.py
@@ -1,5 +1,5 @@
 """
-EL FAMOSO "FIZZ BUZZ"
+EL FAMOSO "FIZZ BUZZ" (Fácil)
 Escribe un programa que muestre por consola (con un print) los
 números de 1 a 100 (ambos incluidos y con un salto de línea entre
 cada impresión), sustituyendo los siguientes:
@@ -23,7 +23,7 @@ fizzbuzz()
 
 
 """
-¿ES UN ANAGRAMA?
+¿ES UN ANAGRAMA? (Media)
 Escribe una función que reciba dos palabras (String) y retorne
 verdadero o falso (Bool) según sean o no anagramas.
 - Un Anagrama consiste en formar una palabra reordenando TODAS
@@ -43,7 +43,7 @@ print(is_anagram("Amor", "Roma"))
 
 
 """
-LA SUCESIÓN DE FIBONACCI
+LA SUCESIÓN DE FIBONACCI (Difícil)
 Escribe un programa que imprima los 50 primeros números de la sucesión
 de Fibonacci empezando en 0.
 - La serie Fibonacci se compone por una sucesión de números en
@@ -67,7 +67,7 @@ fibonacci()
 
 
 """
-¿ES UN NÚMERO PRIMO?
+¿ES UN NÚMERO PRIMO? (Media)
 Escribe un programa que se encargue de comprobar si un número es o no primo.
 Hecho esto, imprime los números primos entre 1 y 100.
 """
@@ -91,7 +91,7 @@ is_prime()
 
 
 """
-INVIRTIENDO CADENAS
+INVIRTIENDO CADENAS (Fácil)
 Crea un programa que invierta el orden de una cadena de texto
 sin usar funciones propias del lenguaje que lo hagan de forma automática.
 - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
@@ -105,3 +105,35 @@ def reverse(text):
     return reverse_text
 
 print(reverse("Hola mundo"))   
+
+
+"""
+ÁREA DE UN POLÍGONO (Fácil)
+Crea una única función (importante que sólo sea una) que sea capaz
+de calcular y retornar el área de un polígono.
+- La función recibirá por parámetro sólo UN polígono a la vez.
+- Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
+- Imprime el cálculo del área de un polígono de cada tipo.
+"""
+
+def poligono(base, height):
+    
+    if base == height:
+        result = base * height
+        print("El area del caudrado es: " + str(result))
+        return result
+    elif base > height:
+        result = base * height
+        print("El area del rectangulo es: " + str(result))
+        return result
+    elif base < height:
+        result = (base * height) / 255
+        print("El area del triangulo es: " + str(result))
+        return result
+    else:
+        print("Esto no es un cuadrado, triangulo o rectangulo")
+
+base = int(input("Base: "))
+height = int(input("Altura: "))
+
+poligono(base, height)
